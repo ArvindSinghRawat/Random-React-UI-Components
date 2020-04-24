807f4a852966f3e14f1c79495fc57b0bf25c8c22
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2020-04-25 00:23:06 +0530

    Added Component Card in Material UI Components.
    - For Demo Purpose : Added Card.
    - Made trailing commas compulsory for Project.
    - Added Card in Demo.

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-import { ExampleComponent } from 'random-react-ui-components'
-import 'random-react-ui-components/dist/index.css'
+import { ExampleComponent, Card } from 'random-react-ui-components';
+import 'random-react-ui-components/dist/index.css';
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
-}
+  return (
+    <div>
+      <ExampleComponent text='Create React Library Example 😄' />
+      <h1>Card : </h1>
+      <Card style={{ width: 500, height: 500, margin: '20px auto' }}>
+        This is My Card
+      </Card>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React from 'react';
+
+// Styles
+import styles from './styles.module.css';
 
 export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+  return <div className={styles.test}>My Custom Component: {text}</div>;
+};
+
+// Components
+export { Card } from './components/material/card/Card';
