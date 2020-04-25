ba9a5fd904caf87edc9319453aa5eac9be8b264d
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2020-04-25 22:16:28 +0530

    Added Card with all types in Example.
    - Added Global.css for global styling.

--- example/src/App.js
+++ example/src/App.js
@@ -2,15 +2,39 @@ import React from 'react';
 
 import { ExampleComponent, Card } from 'random-react-ui-components';
 import 'random-react-ui-components/dist/index.css';
+import styles from './App.module.css';
 
 const App = () => {
+  const commonStyle = {
+    width: 300,
+    height: 300,
+    margin: '15px auto',
+    padding: 'auto'
+  };
+
   return (
     <div>
       <ExampleComponent text='Create React Library Example 😄' />
-      <h1>Card : </h1>
-      <Card style={{ width: 500, height: 500, margin: '20px auto' }}>
-        This is My Card
-      </Card>
+      <h1>Cards</h1>
+      <div className='row'>
+        <Card style={commonStyle} className={styles.VerticalFlexbox}>
+          This is Default Card
+        </Card>
+        <Card
+          type='dark'
+          style={commonStyle}
+          className={styles.VerticalFlexbox}
+        >
+          This is Dark Card
+        </Card>
+        <Card
+          type='xdark'
+          style={commonStyle}
+          className={styles.VerticalFlexbox}
+        >
+          This is Extra Dark Card
+        </Card>
+      </div>
     </div>
   );
 };
