ba3b13eef06eebc28179406b8258562365c63b01
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2020-04-27 00:15:22 +0530

    Readded and Renamed Material/Card.

---
+++ src/components/material/Card/Card.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// Styles
+import styles from './Card.module.css';
+
+const possibleTypes = ['dark', 'xdark'];
+const possibleAnimate = ['shadow'];
+
+export const Card = (props) => {
+  const cssClasses = [styles.Card, styles.ShadowSmooth];
+  if (props.type) {
+    const type = props.type.toLowerCase();
+    switch (type) {
+      case 'dark':
+        cssClasses.push(styles.dark);
+        break;
+      case 'xdark':
+        cssClasses.push(styles.xdark);
+        break;
+      default:
+        console.log(type + ' type not found');
+        break;
+    }
+  }
+  if (props.animate) {
+    const type = props.animate.toLowerCase();
+    switch (type) {
+      case 'shadow':
+        cssClasses.push(styles.ShadowAnimate);
+        break;
+      default:
+        break;
+    }
+  }
+  return (
+    <div
+      className={cssClasses.join(' ') + ' ' + props.className}
+      style={{ ...props.style }}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+Card.propTypes = {
+  type: PropTypes.oneOf(possibleTypes),
+  animate: PropTypes.oneOf(possibleAnimate),
+  className: PropTypes.string,
+  style: PropTypes.object,
+  children: PropTypes.oneOfType([PropTypes.element, PropTypes.node])
+};
+
+export default Card;
